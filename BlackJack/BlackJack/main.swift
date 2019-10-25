792c67f5ce8b63f4451b71d38d10d11bcbb66169
Margiett Gil <margiett@Margietts-MacBook-Pro.local>
2019-10-25 14:35:30 -0400

    working on the file main and creating the repeat

--- BlackJack/BlackJack/main.swift
+++ BlackJack/BlackJack/main.swift
@@ -8,20 +8,29 @@
 
 // faceCards are the jack queen and king and they all equal to 10
 // ace is equal to 1 or 11 i choice
-// suits are the number cards which are 2 ... 10 
+// suits are the number cards which are 2 ... 10
 var userResponse = Bool()
 print(" !!! Welcome to BlackJAck !! ")
 
 print(" Hit or Pass")
 print("There are \(Card.newDeck(aceValue: 1).count) in a deck of cards")
 let game = Game()
+game.newGame() // this will reset the game
+var userPrompt = "Please Choice one (Hit, or Pass)"
+let hit = "hit"
+let pass = "pass"
+
+print(userPrompt)
+
+
 var playAgain = false
 //var userResponse = Bool()
-let userPrompt = "Hit or Pass ??"
+//let userPrompt = "Hit or Pass ??"
 
 repeat {
-    print(userPrompt).readLine() ?? ""
-    let userResponse = readLine() ?? ""
-    game.newGame(userResponse)
+//    print(userPrompt).readLine() ?? ""
+//    let userResponse = readLine() ?? ""
+//    game.newGame(userResponse)
     
 } while false
+
